c8c7a547fbf752a4e2ac2d9e2f06ee913c0d7cfd
jinii9 <jinii915@naver.com>
2024-03-09 18:36:25 +0900

    이진탐색-이론

--- book/ch6-정렬/practice.py
+++ book/ch6-정렬/practice.py
@@ -1,28 +1,6 @@
-# 입력
-n, m = map(int, input().split())
-graph = []
+import sys
+# 하나의 문자열 데이터 입력받기
+input_data = sys.stdin.readline().rstrip()
 
-for i in range(n):
-    graph.append(list(map(int, input())))
-
-def dfs(x, y):
-    if x>=n or x<0 or y>=m or y<0:
-        return False
-
-    if graph[x][y] == 0:
-        graph[x][y] = 1 # 방문 완료
-        dfs(x-1, y)
-        dfs(x+1, y)
-        dfs(x, y-1)
-        dfs(x, y+1)
-        return True
-
-    return False
-
-result = 0
-for i in range(n):
-    for j in range(m):
-        if dfs(i, j) == True:
-            result += 1
-
-print(result)
+# 입력받은 문자열 그대로 출력
+print(input_data)
