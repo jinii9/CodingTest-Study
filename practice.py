fe44584f28c3dc4de8f18ff1e5aacfc729acc0d5
jinii9 <jinii915@naver.com>
2024-03-23 17:51:32 +0900

    최단경로-다익스트라 이론

--- practice.py
+++ practice.py
@@ -1,24 +1,35 @@
-# 입력
-# 2 15
-# 2
-# 3
+import heapq
+import sys
+input = sys.stdin.readline
+INF = int(1e9)
 
-n, m = map(int, input().split())
-arr = []
-for i in range(n):
-    arr.append(int(input()))
+n, m = map(int, input().split()) # 노드, 간선 개수
+start = int(input())
+graph = [[] for i in range(n + 1)]
+distance = [INF] * (n + 1)
 
-d = [10001] * (m + 1)
-# 인덱스 : 금액
-# 인덱스 해당 값 : 최소 화폐 개수
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
 
-d[0] = 0
-for i in range(n): # 화폐 단위
-    for j in range(arr[i], m + 1):
-        if d[j-arr[i]] != 10001:
-            d[j] = min(d[j], d[j-arr[i]] + 1)
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start)) # 거리, 노드
+    distance[start] = 0
+    while q:
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
 
-if d[m] == 10001:
-    print(-1)
-else:
-    print(d[m])
+dijkstra(start)
+
+for i in range(1, n + 1):
+    if distance[i] == INF:
+        print("INF")
+    else:
+        print(distance[i])
