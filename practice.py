963b1a5b8593a368952101bde5bf82dd3ab8fbbd
jinii9 <jinii915@naver.com>
2024-05-20 23:36:48 +0900

    dfs/bfs-미로 탈출(re)

--- practice.py
+++ practice.py
@@ -1,27 +1,33 @@
+from collections import deque
+
+# 미로탈출
 n, m = map(int, input().split())
 graph = []
-
 for i in range(n):
     graph.append(list(map(int, input())))
 
-def dfs(x, y):
-    if x<0 or x>=n or y<0 or y>=m:
-        return False
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-    if graph[x][y] == 0:
-        graph[x][y] = 1
-        dfs(x-1, y) # 상
-        dfs(x+1, y) # 하
-        dfs(x, y-1) # 좌
-        dfs(x, y+1) # 우
-        return True
+def bfs(x, y):
+    queue = deque([(x, y)])
 
-    return False
+    while queue:
+        x, y = queue.popleft()
 
-result = 0
-for i in range(n):
-    for j in range(m):
-        if dfs(i, j) == True:
-            result += 1
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
 
-print(result)
+            if nx<0 or ny<0 or nx>=n or ny>=m:
+                continue
+            if graph[nx][ny] == 0:
+                continue
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
+
+    return graph[n-1][m-1]
+
+
+print(bfs(0, 0))
