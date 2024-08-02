1dcf75e555155e0451ce3ed2b73443dc316edb98
jinii9 <jinii915@naver.com>
2024-08-02 12:52:49 +0900

    p-해시:전화번호 목록

---
+++ programmers/전화번호 목록.py
@@ -0,0 +1,16 @@
+def solution(phone_book):
+    # 1. Hash map 생성
+    hash_map = {}
+    for nums in phone_book:
+        hash_map[nums] = 1
+    
+    # 2. 접두어가 Hash map에 존재하는지 찾기
+    for nums in phone_book:
+        if nums in hash_map and nums
+            
+            # 3. 본인 자체일 경우는 제외
+            if arr in hash_map and arr != nums:
+                print("arr", arr)
+                return False
+            
+    return True
