ba9e9a26fb305a373400b25ff0a31b4059289283
jinii9 <jinii915@naver.com>
2024-08-22 18:41:17 +0900

    (re)boj-2503 숫자 야구

--- baekjoon/숫자 야구.py
+++ baekjoon/숫자 야구.py
@@ -1,48 +1,46 @@
-# 21~
-# 영수: 1~9 서로 다른 숫자 3개
-# 동일한 위치 - 스트라이크
+# 동일한 자리에 위치 - 스트라이크
 # 다른 자리에 위치 - 볼
-# 3스타라이크 정답
+# 324
+# 429 : 1s 1b
+# 241 : 0s 2b
+# 924 : 2s 0b
+# 3s이면 게임 끝
 
-# 영수가 생각하고 있을 가능성 있는 수가 총 몇 개인지 맞취기
-# 민혁이가 질문한 세자리 수, 스트라이크 개수, 볼의 개수
+
+# 가능성 있는 수 개수 맞추기
+# 질문수 N
+# 수 S개수 B개수
+
+# 브루트포스 : 100
+# _ _ _ 9*8*7 = 9P3
 
 
-##
-# 1 2 3 4 8 9
-# 1,123    1,356    2,327
+# !! 120 130의 각 자릿수를 비교하기 위해서는 string이어야 한다 !!
 
 from itertools import permutations
 
 N = int(input())
-arr = [ list(map(int, input().split())) for _ in range(N) ]
-count = 0
+arr = [input().split() for _ in range(N)]
 answer = 0
 
-for permutation in permutations(range(1, 10), 3):
-    for x in arr:
-        strike_count = 0
-        ball_strike = 0
 
-        for i, per in enumerate(permutation):
-            # 스트라이크
-            if per == int(str(x[0])[i]):
-                strike_count += 1  
-            # 볼
-            if str(per) in str(x[0]):
-                ball_strike += 1
+for perm in permutations(range(1,10), 3):
+    check = True
 
-        ball_strike -= strike_count
-        if x[1]==strike_count and x[2]==ball_strike:
-            count += 1
-        else:
+    for n, s, b in arr:
+        b_count = s_count  = 0
+
+        for i in range(3):
+            if str(perm[i]) == n[i]:
+                s_count += 1
+            elif str(perm[i]) in n:
+                b_count += 1
+
+        if s_count != int(s) or b_count != int(b):
+            check = False
             break
     
-    if count == N:
-        # answer.append(permutation)
+    if check:
         answer += 1
-    
-    count = 0
-
 
 print(answer)
--- practice.py
+++ practice.py
@@ -1,29 +1,46 @@
-# LCS
-# ACAYKP
-# CAPCAK
-# d[n][m]
-# ABDEA
-# DAWEF
-# 같을 경우 : d[n-1][m-1] + 1
-# 다를 경우 : d[n-1][m], d[n][m-1]
+# 동일한 자리에 위치 - 스트라이크
+# 다른 자리에 위치 - 볼
+# 324
+# 429 : 1s 1b
+# 241 : 0s 2b
+# 924 : 2s 0b
+# 3s이면 게임 끝
 
-S1 = input()
-S2 = input()
 
-N, M = len(S1), len(S2)
-s1 = " " + S1
-s2 = " " + S2
+# 가능성 있는 수 개수 맞추기
+# 질문수 N
+# 수 S개수 B개수
 
-dp = [[0] * (M+1) for _ in range(N+1)]
+# 브루트포스 : 100
+# _ _ _ 9*8*7 = 9P3
 
-# 초기값
-#
 
-for i in range(1, N+1):
-    for j in range(1, M+1):
-        if S1[i] == S2[j]:
-            dp[i][j] = dp[i-1][j-1] + 1
-        else:
-            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+# !! 120 130의 각 자릿수를 비교하기 위해서는 string이어야 한다 !!
 
-print(dp[N][M])
+from itertools import permutations
+
+N = int(input())
+arr = [input().split() for _ in range(N)]
+answer = 0
+
+
+for perm in permutations(range(1,10), 3):
+    check = True
+
+    for n, s, b in arr:
+        b_count = s_count  = 0
+
+        for i in range(3):
+            if str(perm[i]) == n[i]:
+                s_count += 1
+            elif str(perm[i]) in n:
+                b_count += 1
+
+        if s_count != int(s) or b_count != int(b):
+            check = False
+            break
+    
+    if check:
+        answer += 1
+
+print(answer)
